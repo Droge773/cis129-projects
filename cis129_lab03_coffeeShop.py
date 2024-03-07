615c96cf24748954e3bbf48d045c0d8b6ffe9e07
Droge773 <160818278+Droge773@users.noreply.github.com>
2024-03-06 21:10:02 -0700

    Create cis129_lab03_coffeeShop.py

---
+++ cis129_lab03_coffeeShop.py
@@ -0,0 +1,37 @@
+##Author: Eve Droge
+##This code is meant to mimick the following example output text as well as allow similar depicted user interaction
+##***************************************
+##My Coffee and Muffin Shop
+##Number of coffees bought?
+##1
+##Number of muffins bought?
+##2
+##***************************************
+##
+##***************************************
+##My Coffee and Muffin Shop Receipt
+##1 Coffee at $5 each: $ 5.00
+##2 Muffins at $4 each: $ 8.00
+##6% tax: $ .78
+##---------
+##Total: $ 13.78
+##***************************************
+
+#I like making really annoying to read print statements to print blocks of statements
+print("***************************************\nMy Coffee and Muffin Shop\nNumber of coffees bought?\n")
+#Both of these create variables for the numbers of coffees and muffins respectively that the user wants
+coffees = input()
+muffins = input("\nNumber of muffins bought?\n")
+#Trial and error has reminded me that C and Python don't treat variables similar
+#therefore I accidentally made the following block of strings repeat the number of coffees 5 times instead of printing that value times five.
+#coding is a funny thing sometimes
+#The next lines convert the inputs to integers, and find values for their prices
+coffees = int(coffees)
+muffins = int(muffins)
+coffeePrice = coffees * 5
+muffinsPrice = muffins * 4
+print("***************************************\n\n***************************************\nMy Coffee and Muffin Shop Receipt\n" + str(coffees) + " Coffee @ $5 each: $ " + str(coffeePrice) + ".00\n" + str(muffins) + " Muffins @ $4 each: $ " + str(muffinsPrice) + ".00\n")
+tax = 0.06 * (muffinsPrice+coffeePrice) #determines the sales tax on coffee
+print("6% tax: $" + str(tax)) #ngl, I get nervous when projects like this say anything about appropriate documentation because I never know if teachers want me to like comment ever print statement or just the calculations going on
+print("\n---------\nTotal: $ " + str(tax+muffinsPrice+coffeePrice))
+print("\n***************************************")
